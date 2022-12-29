2b8361b3f47c4b62629e9d8871352ca7445d24e7
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-29 12:02:30 +0900

    1.Controller와 템플릿 만들기 / 마크다운 작성

---
+++ board/Readme.md
@@ -0,0 +1,23 @@
+# 게시판 만들기
+
+##사용된 기술
+
+-Spring Boot
+-Spring MVC
+-Spring JDBC
+-MySQL
+-thymeleaf
+
+## 아키텍처
+```
+                      Spring Core
+                      Spring MVC             Spring JDBC  MySQL     
+브라우저 --- 요청 ---> Controller ---> Service ---> DAO --->DB
+       <--- 응답---템플릿         <---         <---     <---
+                      <---Layer간에 데이터 전송은 DTO로 한다-->
+```
+
+## 게시판 만드는 순서
+1.Controller와 템플릿
+2.Service - 비즈니스 로직을 처리(하나의 Tx단위로 동작)
+3.Service는 비즈니스로직을 처리하기위해 CRUD 하기위해 DAO사용
--- board/src/main/java/com/example/board/controller/BoardController.java
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,5 +16,28 @@ public class BoardController {
     @GetMapping("/board")
     public String board(@RequestParam("id") int id) {
         return "board";
+    }
+
+    @GetMapping("/writeForm")
+    public String writeForm(){
+//        로그인 한 사용자만 글을 써야한다.
+//        세션에서 로그인 한 정보를 읽어들인다. 로그인을 하지 않았다면 list보기로 자동이동.
+//        로그인 한 정보 +
+        return "writeForm";
+    }
+
+    @PostMapping("/write")
+    public String write(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content
+    ) {
+//        로그인한 사용자만 글을 써여한다.
+//        세션에서 로그인한 정보를 읽어들인다. 로그인을 하지 않았다면 리스트보기로 자동 이동 시킨다.
+        System.out.println("title : " + title);
+        System.out.println("content :" + content);
+//        로그인 한 회원 정보 + 제목, 내용을 저장한다.
+
+        return "redirect:/"; // 리스트 보기로 리다이렉트한다.
+
     }
 }
